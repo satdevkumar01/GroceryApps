20eedabb90c45a0e5b95f5ec3f8e9845a233815f
satdevkumar <satdevkumar014@gmail.com>
2025-04-26 22:36:36 +0530

    error fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,15 +9,14 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
     }
+
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
     }
 }
 
