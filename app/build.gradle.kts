0045acaff6c5a9749b7b0cf3c400b9506a70d549
satdevkumar <satdevkumar014@gmail.com>
2025-04-26 23:39:15 +0530

    error fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.sokhal.groceryapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.sokhal.groceryapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
